1c2820a19d49304e6f3ac0acec82b03f32e44b06
Lukas <lukas.soderlund.fanta@gmail.com>
2025-09-25 12:01:06 +0200

    I feel like this collision are to complex

--- collision.js
+++ collision.js
@@ -10,6 +10,19 @@ export function handleCollision(player, currentX, currentY, velocityY, ) {
          const playerRight = currentX + playerWidth
          const playerLeft = currentX
 
+        const obstacleTop = obstacle.y
+        const obstacleBottom = obstacle.y + obstacle.height
+        const obstacleLeft = obstacle.x
+        const obstacleRight = obstacle.x + obstacle.width
+
+        //Standing on obstacle (on Top)
+
+        if( playerBottom >= obstacleTop && playerBottom <= obstacleTop + 20 && playerRight > obstacleLeft && playerLeft < obstacleRight && velocityY >= 0){
+            currentY = obstacleTop - playerHeight
+            velocityY = 0
+            // and not jumping
+        }
+
 
     }
 
