c9d2c47cd12f7bcb5bbe0be53e30c1fe4693a8aa
Lukas <lukas.soderlund.fanta@gmail.com>
2025-09-26 12:08:02 +0200

    scene system to create diffrent lvls

---
+++ scene.js
@@ -0,0 +1,41 @@
+// scene.js
+import { obstacles, coins } from "./obstacle.js"
+import { resetScore } from "./score.js"
+
+let scenes = []
+let currentSceneIndex = 0
+
+export function addScene(sceneFn) {
+    scenes.push(sceneFn)
+}
+
+export function setScene(index) {
+    if (index < 0 || index >= scenes.length) return
+
+    // clear old scene
+    for (const obj of obstacles) {
+        if (obj.obstacle) obj.obstacle.remove()
+    }
+    obstacles.length = 0
+
+    for (const coin of coins) {
+        if (coin.element) coin.element.remove()
+    }
+    coins.length = 0
+
+    currentSceneIndex = index
+    resetScore()
+    scenes[currentSceneIndex]()
+}
+
+export function nextScene() {
+    if (currentSceneIndex + 1 < scenes.length) {
+        setScene(currentSceneIndex + 1)
+    } else {
+        console.log("No more scenes")
+    }
+}
+
+export function getCurrentSceneIndex() {
+    return currentSceneIndex
+}
