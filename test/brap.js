5afe1fd3a1c27dfd950815b5d4f2f546ea179af4
Lukas <lukas.soderlund.fanta@gmail.com>
2025-09-27 12:52:15 +0200

    made keys to private fields

--- test/brap.js
+++ test/brap.js
@@ -1,52 +1,55 @@
+// PlayerInput.js
 export class PlayerInput {
-  constructor(controlScheme = 'wasd') {
-    this.controlScheme = controlScheme
-    this.left = false
-    this.right = false
-    this.up = false
-    this.down = false
-    this.jumpPressed = false
+  #scheme
+  #left = false
+  #right = false
+  #up = false
+  #down = false
+  #jump = false
 
+  constructor(scheme = 'wasd') {
+    this.#scheme = scheme
     this.handleKeyDown = this.handleKeyDown.bind(this)
     this.handleKeyUp = this.handleKeyUp.bind(this)
-
     document.addEventListener('keydown', this.handleKeyDown)
     document.addEventListener('keyup', this.handleKeyUp)
   }
 
   handleKeyDown(event) {
     const key = event.key.toLowerCase()
-    if (this.controlScheme === 'wasd') {
-      if (key === 'a') this.left = true
-      if (key === 'd') this.right = true
-      if (key === 's') this.down = true
-      if (key === 'w') this.up = true
-      if (key === ' ') this.jumpPressed = true
-    } else if (this.controlScheme === 'platform') {
-      if (key === 'a') this.left = true
-      if (key === 'd') this.right = true
-      if (key === ' ') this.jumpPressed = true
+    if (this.#scheme === 'wasd') {
+      if (key === 'a') this.#left = true
+      if (key === 'd') this.#right = true
+      if (key === 's') this.#down = true
+      if (key === 'w') this.#up = true
+      if (key === ' ') this.#jump = true
+    } else if (this.#scheme === 'platform') {
+      if (key === 'a') this.#left = true
+      if (key === 'd') this.#right = true
+      if (key === ' ') this.#jump = true
     }
   }
 
   handleKeyUp(event) {
     const key = event.key.toLowerCase()
-    if (this.controlScheme === 'wasd') {
-      if (key === 'a') this.left = false
-      if (key === 'd') this.right = false
-      if (key === 's') this.down = false
-      if (key === 'w') this.up = false
-      if (key === ' ') this.jumpPressed = false
-    } else if (this.controlScheme === 'platform') {
-      if (key === 'a') this.left = false
-      if (key === 'd') this.right = false
-      if (key === ' ') this.jumpPressed = false
+    if (this.#scheme === 'wasd') {
+      if (key === 'a') this.#left = false
+      if (key === 'd') this.#right = false
+      if (key === 's') this.#down = false
+      if (key === 'w') this.#up = false
+      if (key === ' ') this.#jump = false
+    } else if (this.#scheme === 'platform') {
+      if (key === 'a') this.#left = false
+      if (key === 'd') this.#right = false
+      if (key === ' ') this.#jump = false
     }
   }
 
-  playerWantsToGoLeft()  { return this.left }
-  playerWantsToGoRight() { return this.right }
-  playerWantsToGoUp()    { return this.up }
-  playerWantsToGoDown()  { return this.down }
-  playerWantsToJump()    { return this.jumpPressed }
+  playerWantsToGoLeft()  { return this.#left }
+  playerWantsToGoRight() { return this.#right }
+  playerWantsToGoUp()    { return this.#up }
+  playerWantsToGoDown()  { return this.#down }
+  playerWantsToJump()    { return this.#jump }
+  get controlScheme()    { return this.#scheme }
+
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,3 @@
 import "./scriptWithClassTest.js"
 
-window.startPlayer('platform', {speed: 20, gravity: 0.6, jumpStrength: 38})
+window.startPlayer('platform', {speed: 20, gravity: 0.6, jumpStrength: 28})
